b9aa276bafe0ad382aa929dd2d5dbb2b8fcdec04
Ilya Voinov <ilya.voinov@yahoo.com>
2024-02-21 11:38:46 +0100

    Move celery broker url to the env variable

--- celery_app/celery_worker.py
+++ celery_app/celery_worker.py
@@ -1,7 +1,10 @@
 from celery import Celery
+import os
 
+# Get broker URL from environment variables
+broker_url = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
 # Create a Celery app
-celery = Celery("celery_worker", broker="redis://localhost:6379/0")
+celery = Celery("celery_worker", broker=broker_url)
 celery.conf.update(
     broker_connection_retry=True,
     broker_connection_retry_on_startup=True,
