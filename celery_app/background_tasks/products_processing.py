98a28336c94708a3223faf0c12a96a9fd9514454
Ilya Voinov <ilya.voinov@yahoo.com>
2024-02-22 16:51:09 +0100

    Update products processing task

--- celery_app/background_tasks/products_processing.py
+++ celery_app/background_tasks/products_processing.py
@@ -13,17 +13,21 @@ def process_products_csv_file():
             # Process each row of the CSV file
             sku = row.get("sku", "")
             name = row.get("name", "")
-            description = row.get("product_description", "")
+            short_description = row.get("product_description", "")
+            description = row.get("full_description_text_translated", "")
             price = float(row.get("price", 0))
             categories_names = row.get("categories_names", "")
             current_price = float(row.get("current_price", 0))
+            parent_category = int(row.get("parent_category", ""))
             # Create a new Product instance
             product = Product(
                 sku=sku,
                 name=name,
                 price=price,
                 description=description,
+                short_description=short_description,
                 categories_names=categories_names,
+                parent_category=parent_category,
                 current_price=current_price,
             )
             create_or_update_product(product)
