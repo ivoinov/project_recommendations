0057343ed3af569a97a8e18e364c84ed2e716bcb
Ilya Voinov <ilya.voinov@yahoo.com>
2024-02-23 17:32:58 +0100

    Add get similar product API endpoint

--- app/routers/recommendations.py
+++ app/routers/recommendations.py
@@ -1,11 +1,13 @@
 from .schemas import ProductRecommendationResponse
 from app.models import ProductRecommendation, Token
-import joblib
+import os.path
+import pickle
 from fastapi import Depends, HTTPException, status, APIRouter
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from app.database import get_db
+from app.database import get_db, get_product_by_sku, get_product_skus_by_ids
 from datetime import datetime
+from sklearn.metrics.pairwise import linear_kernel
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -29,15 +31,55 @@ router = APIRouter(dependencies=[Depends(verify_token)])
 
 
 @router.get(
-    "/upselling-recommendations/{product_id}",
+    "/upselling-recommendations/{product_sku}",
     response_model=ProductRecommendationResponse,
 )
 def upselling_recommendations(
-    product_id: int, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+    product_sku: int, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
 ):
     # Verify the token
     verify_token(token, db)
     # Recommendation logic based on `product_similarity` and `index_to_product_id`
     return ProductRecommendation(
-        product_id=product_id, recommendations=[1, 2, 3]
+        product_sku=product_sku, recommendations=[1, 2, 3]
     )  # Mocked response
+
+
+@router.get(
+    "/similar-recommendations/{product_sku}",
+    response_model=ProductRecommendationResponse,
+)
+def similar_recommendations(
+    product_sku: str, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    # Verify the token
+    verify_token(token, db)
+    if os.path.isfile("tfidf_matrices.pkl"):
+        # Load saved TF-IDF matrices and vectorizers from file
+        with open("tfidf_matrices.pkl", "rb") as file:
+            tfidf_matrices = pickle.load(file)
+            product = get_product_by_sku(product_sku)
+            if product.parent_category in tfidf_matrices:
+                tfidf_matrix = tfidf_matrices[product.parent_category]["tfidf_matrix"]
+                tfidf_vectorizer = tfidf_matrices[product.parent_category][
+                    "tfidf_vectorizer"
+                ]
+                # Compute TF-IDF matrix for the product description
+                product_tfidf = tfidf_vectorizer.transform([product.description])
+                # Compute the cosine similarity between the product and all other products
+                cosine_similarities = linear_kernel(
+                    product_tfidf, tfidf_matrix
+                ).flatten()
+                # Get the index of the most similar products
+                similar_indices = cosine_similarities.argsort()[:-6:-1]
+                internal_ids = []
+                for index, value in enumerate(similar_indices):
+                    internal_ids.append(value)
+                # Get the SKUs of the most similar products
+                similar_product_skus = [
+                    product.sku for product in get_product_skus_by_ids(internal_ids)
+                ]
+                return ProductRecommendation(
+                    product_sku=product_sku, recommendations=similar_product_skus
+                )
+    return ProductRecommendation(product_sku=product_sku, recommendations=[])
