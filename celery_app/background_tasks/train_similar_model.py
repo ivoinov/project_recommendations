ceb4a96d3e976b320e0e136bce809a3173b7d1e4
Ilya Voinov <ilya.voinov@yahoo.com>
2024-03-05 14:58:09 +0100

    Refactor train similar model

--- celery_app/background_tasks/train_similar_model.py
+++ celery_app/background_tasks/train_similar_model.py
@@ -1,32 +1,28 @@
 import pandas as pd
-from app.database import get_product_by_categories
+from app.database import get_all_products
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
-import os.path
-from sklearn.metrics.pairwise import linear_kernel
+import os
 
 
 def train_similar_model():
-    # TODO:: Implement recalculate logic. Implement entity to store version, file name and recalculate logic.
-    recalculate = True
-    if not recalculate and os.path.isfile("tfidf_matrices.pkl"):
-        # Load saved TF-IDF matrices and vectorizers from file
-        with open("tfidf_matrices.pkl", "rb") as file:
-            tfidf_matrices = pickle.load(file)
-        return tfidf_matrices
+    global tfidf_matrices
     tfidf_matrices = {}
-    products_by_category = get_product_by_categories()
-    for cat, descriptions in products_by_category:
-        # Extract descriptions from the fetched products
-        descriptions = [desc for desc in descriptions if desc]
-        # Initialize a TF-IDF vectorizer
+    # TODO:: Implement recalculate logic. Implement entity to store version, file name and recalculate logic.
+    df_total = pd.DataFrame([product.as_dict() for product in get_all_products()])
+    category_list = set(df_total["parent_category"])
+    for cat in category_list:
+        df_cat = df_total[df_total["parent_category"] == cat]
         tfidf = TfidfVectorizer(stop_words="english")
-        # Compute TF-IDF matrix for the descriptions
-        tfidf_matrix = tfidf.fit_transform(descriptions)
-        # Store TF-IDF matrix and vectorizer in the tfidf_matrices dictionary
+        df_cat = df_cat.copy()
+        # Concatenate description and short_description into a new column
+        df_cat["combined_description"] = (
+            df_cat["description"].fillna("")
+            + " "
+            + df_cat["short_description"].fillna("")
+        )
+        # df_cat['description'] = df_cat['description'].fillna(" ")
+        tfidf_matrix = tfidf.fit_transform(df_cat["combined_description"])
         tfidf_matrices[cat] = {"tfidf_matrix": tfidf_matrix, "tfidf_vectorizer": tfidf}
-        # Save TF-IDF matrices and vectorizers to file
-        with open("tfidf_matrices.pkl", "wb") as file:
-            pickle.dump(tfidf_matrices, file)
-
-    return tfidf_matrices
+    with open("product_recommender.pkl", "wb") as f:
+        pickle.dump(tfidf_matrices, f)
