799e050aca070dd3de26a95e466fd606e4febc7f
Ilya Voinov <ilya.voinov@yahoo.com>
2024-03-06 14:26:39 +0100

    Use price vectors by categories

--- app/config.py
+++ app/config.py
@@ -3,9 +3,10 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     description_tfidf_matrices: dict = {}
-    price_vector: list = []
+    price_vectors: dict = {}
     description_tfidf_file_name: str = "description_tfidf.pkl"
     price_vector_file_name: str = "price_vector.pkl"
 
 
+global settings
 settings = Settings()
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from app.database import SessionLocal, create_tables
 from contextlib import asynccontextmanager
 from celery_app.background_tasks.train_similar_model import (
     load_description_matrices,
-    load_price_vector,
+    load_price_vectors,
 )
 from celery_app.celery_worker import celery
 from app.config import settings
@@ -15,7 +15,7 @@ from app.config import settings
 async def lifespan(app: FastAPI):
     try:
         settings.description_tfidf_matrices = load_description_matrices()
-        settings.price_vector = load_price_vector()
+        settings.price_vectors = load_price_vectors()
         yield
     finally:
         pass
@@ -43,3 +43,6 @@ app.add_middleware(
 
 # Create all tables in the database
 create_tables()
+
+# Declare the Celery app
+celery_app = celery
