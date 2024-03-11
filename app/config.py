422f03b7fcfbd8e3c36c5479a909a02ef5318483
Ilya Voinov <ilya.voinov@yahoo.com>
2024-03-11 09:46:33 +0100

    Update the configuration

--- app/config.py
+++ app/config.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings
 import os, logging
 from logging import Logger
 from dotenv import load_dotenv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 load_dotenv()
 
@@ -9,7 +11,6 @@ load_dotenv()
 class Settings(BaseSettings):
     project_root: str = os.getcwd()
     logger: Logger = logging.getLogger(__name__)
-    DATABASE_URL: str = str(os.getenv("DATABASE_URL"))
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
     SECRET_KEY: str = str(os.getenv("SECRET_KEY"))
     ALGORITHM: str = "HS256"
@@ -19,5 +20,20 @@ class Settings(BaseSettings):
     price_vector_file_name: str = "price_vector.pkl"
 
 
-global settings
+class Database(BaseSettings):
+    DATABASE_URL: str = str(os.getenv("DATABASE_URL"))
+
+    def get_db(self):
+        SessionLocal = sessionmaker(
+            autocommit=False, autoflush=False, bind=create_engine(self.DATABASE_URL)
+        )
+        db = SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+
+
+global settings, db_settings
 settings = Settings()
+db_settings = Database()
