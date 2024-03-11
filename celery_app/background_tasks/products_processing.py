e4cb4c74b4e376e0a5a25fd5c795e742d8ec455f
Ilya Voinov <ilya.voinov@yahoo.com>
2024-03-11 11:09:47 +0100

    Update product processing according to the new services

--- celery_app/background_tasks/products_processing.py
+++ celery_app/background_tasks/products_processing.py
@@ -1,42 +1,39 @@
 import csv, os
-from app.config import settings
-from app.models import Product
+from app.config import settings, db_settings
+from app.services import ProductService
+from app.repositories import ProductRepository
+from sqlalchemy.orm import Session
+from fastapi import Depends
 
 
-def process_products_csv_file():
+def process_products_csv_file(db: Session = Depends(db_settings.get_db)):
     file_path = os.path.join(settings.project_root, "var", "products_data.csv")
     with open(file_path, "r") as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
             try:
+                product = {}
                 # Process each row of the CSV file
-                sku = row.get("sku", "")
-                name = row.get("product_name", "")
-                short_description = row.get("short_description", "")
-                description = row.get("description", "")
-                categories_names = row.get("categories", "")
-                price = (
+                product.sku = row.get("sku", "")
+                product.name = row.get("product_name", "")
+                product.short_description = row.get("short_description", "")
+                product.description = row.get("description", "")
+                product.categories_names = row.get("categories", "")
+                product.price = (
                     float(row.get("price", 0)) if is_float(row.get("price", 0)) else 0.0
                 )
-                current_price = (
+                product.current_price = (
                     float(row.get("price", 0)) if is_float(row.get("price", 0)) else 0.0
                 )
                 # Split the categories_names string by comma and get the last element
-                parent_category = (
-                    categories_names.split(",")[-1].strip() if categories_names else ""
+                product.parent_category = (
+                    product.categories_names.split(",")[-1].strip()
+                    if product.categories_names
+                    else ""
                 )
-                # Create a new Product instance
-                product = Product(
-                    sku=sku,
-                    name=name,
-                    price=price,
-                    description=description,
-                    short_description=short_description,
-                    categories_names=categories_names,
-                    parent_category=parent_category,
-                    current_price=current_price,
-                )
-                create_or_update_product(product)
+                product_repository = ProductRepository(db)
+                product_service = ProductService(product_repository)
+                product_service.create_or_update_product()
             except Exception as e:
                 print(f"Error processing row: {row}")
                 print(e)
